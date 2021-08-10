acffe7793f2c973cd6796f603fa5508562a96046
wesley.menezes <wesley.menezes@dataprev.gov.br>
2021-08-10 12:06:37 -0300

    Fixed restarting process

--- cloudflare_tunnel/rootfs/etc/cont-init.d/cloudflare.sh
+++ cloudflare_tunnel/rootfs/etc/cont-init.d/cloudflare.sh
@@ -3,20 +3,29 @@
 set -eE
 
 if bashio::config.is_empty 'ingress[0].hostname'; then
+  rm -f /data/cert.pem
   bashio::exit.nok "Hostname not defined"
 fi
 
 mkdir -p $HOME/.cloudflared
 cp -r /data/. $HOME/.cloudflared
+
 ls -R $HOME/.cloudflared
+
+yq eval -P /data/options.json > /data/options.yml
+
+cloudflared tunnel ingress validate --config /data/options.yml
+
+cloudflared update
 
 if ! bashio::fs.file_exists "$HOME/.cloudflared/cert.pem"; then
   cloudflared tunnel login
+
+  cloudflared tunnel list
+  TUNNEL=$(bashio::config 'tunnel')
+  cloudflared tunnel delete -f "$TUNNEL"
+  rm -rf $HOME/.cloudflared/*.json
+  cloudflared tunnel create "$TUNNEL"
+
+  cp -r $HOME/.cloudflared/. /data/
 fi
-
-TUNNEL=$(bashio::config 'tunnel')
-cloudflared tunnel delete -f "$TUNNEL"
-rm -rf $HOME/.cloudflared/*.json
-cloudflared tunnel create "$TUNNEL"
-
-cp -r $HOME/.cloudflared/. /data/
