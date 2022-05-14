626c5f90b4963e88974c4e301851832f9e0d74b0
w35l3y <w35l3y@brasnet.org>
2022-05-14 14:50:57 -0300

    Add Apparmor

--- cloudflare_tunnel/rootfs/etc/cont-init.d/cloudflare.sh
+++ cloudflare_tunnel/rootfs/etc/cont-init.d/cloudflare.sh
@@ -22,33 +22,46 @@ cloudflared tunnel --config=/data/options.json ingress validate || bashio::exit.
 CLOUDFLARE_PATH="$HOME/.cloudflared"
 CREDFILE="$CLOUDFLARE_PATH/tunnel.json"
 
-mkdir -p "$CLOUDFLARE_PATH"
+echo "Creating folder... $CLOUDFLARE_PATH"
+mkdir -p -m $(umask -S) "$CLOUDFLARE_PATH"
+
+echo "Copying data..."
 cp -r /data/. "$CLOUDFLARE_PATH"
 
-ls -R "$CLOUDFLARE_PATH"
+echo "Listing directories and files..."
+ls -AgiR "$HOME"
 
+echo "Checking old tunnel..."
 NEW_TUNNEL=$(bashio::config "tunnel")
 OLD_TUNNEL="$NEW_TUNNEL"
 if bashio::fs.file_exists "$CREDFILE"; then
   OLD_TUNNEL=$(bashio::jq "$CREDFILE" ".TunnelName")
 fi
 
+echo "Checking certificate..."
 # certificate not found or tunnel changed name
 if ! bashio::fs.file_exists "$CLOUDFLARE_PATH/cert.pem" || [[ "$OLD_TUNNEL" != "$NEW_TUNNEL" ]] || ! bashio::fs.file_exists "$CREDFILE"; then
+  echo "Logging..."
   cloudflared tunnel login
 
+  echo "Listing tunnels..."
   cloudflared tunnel list
 
+  echo "Deleting tunnels..."
   cloudflared tunnel delete -f "$OLD_TUNNEL" || true
   cloudflared tunnel delete -f "$NEW_TUNNEL" || true
 
+  echo "Removing file..."
   rm -f "$CREDFILE"
 
+  echo "Creating tunnel..."
   cloudflared --credentials-file="$CREDFILE" tunnel create "$NEW_TUNNEL"
 
+  echo "Copying data..."
   cp -r $CLOUDFLARE_PATH/. /data/
 fi
 
+echo "Adding routes..."
 for index in $(bashio::config "ingress|keys"); do
   HOSTNAME=$(bashio::config "ingress[${index}].hostname")
 
